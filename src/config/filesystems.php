39fd3390941df59fe179f69ea19290183c9140ee
Dima <makushin.dima@gmail.com>
2019-01-08 18:27:30 +0200

    up: confige filesystem (save image)

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -44,7 +44,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root'   => storage_path('app'),
+            'root'   => public_path(),
         ],
 
         'public' => [
