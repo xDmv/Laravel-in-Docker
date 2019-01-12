2bc1e28cb2513abeb13f0f956423177de62ac19b
Dima <makushin.dima@gmail.com>
2019-01-12 22:49:50 +0200

    up: new route and main stranica

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,5 +11,28 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+ mix.styles([
+	'resources/assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css',
+	'resources/assets/admin/bower_components/font-awesome/css/font-awesome.min.css',
+	'resources/assets/admin/bower_components/Ionicons/css/ionicons.min.css',
+	'resources/assets/admin/dist/css/AdminLTE.min.css',
+	'resources/assets/admin/dist/css/skins/_all-skins.min.css',
+	'resources/assets/admin/bower_components/morris.js/morris.css',
+	'resources/assets/admin/bower_components/jvectormap/jquery-jvectormap.css',
+	'resources/assets/admin/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
+	'resources/assets/admin/bower_components/bootstrap-daterangepicker/daterangepicker.css',
+	'resources/assets/admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'
+], 'public/css/admin.css');
+
+mix.scripts([
+	'resources/assets/admin/js/jquery.min.js',
+	'resources/assets/admin/js/bootstrap.min.js',
+	'resources/assets/admin/js/fastclick.js',
+	'resources/assets/admin/js/adminlte.min.js',
+	'resources/assets/admin/js/demo.js',
+	'resources/assets/admin/js/ckeditor.js',
+	'resources/assets/admin/js/bootstrap3-wysihtml5.all.min.js'
+], 'public/js/admin.js');
+
+mix.copy('resources/assets/admin/bower_components/font-awesome/fonts','public/fonts');
+mix.copy('resources/assets/admin/bower_components/bootstrap/fonts','public/fonts');
